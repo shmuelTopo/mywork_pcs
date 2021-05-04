120e7df32e013691fd09876348af6c14bf4a6793
toporowitch <4158847@gmail.com>
2021-05-04 13:47:14 -0400

    inclass 43

---
+++ python/43/app.py
@@ -0,0 +1,14 @@
+print("Hello world")
+x = 5
+print(x, type(x))
+x2 = 'Hello'
+print(x, type(x))
+y = True
+print(y, type(y))
+z = 1.3
+print(z, type(z))
+# this a comment
+my_long_variable_name = 5
+my_very_very_long_int = 35353538685389357985789578924789432789547894378954754889758975897598798
+print(my_very_very_long_int, type(my_very_very_long_int))
+print(id(my_very_very_long_int))
